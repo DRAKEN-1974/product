4fc92fcd0e8fb0d49603866d90b0a2eb5831b34c
DRAKEN-1974 <150982428+DRAKEN-1974@users.noreply.github.com>
2025-10-02 09:43:44 +0000

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -113,7 +113,7 @@ export default function Footer() {
   <div className="skillancer-content">
     <img src="/mn.png" alt="Skillancer Logo" className="skillancer-logo" />
     <p className="skillancer-text">
-      Developed By: <strong>M.N.NANDINI INVENTION PRIVATE LIMITED</strong>
+      Developed For: <strong>M.N.NANDINI INVENTION PRIVATE LIMITED</strong>
     </p>
   </div>
 </div>
